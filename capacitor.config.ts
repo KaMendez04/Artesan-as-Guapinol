ea3ea9f934ab48ab8315f0b0295d44433feab704
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2026-03-23 00:14:58 -0600

    feat/se agrego para volverlo una aplicación

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.artesaniasguapinol.app',
+  appName: 'Artesanias Guapinol',
+  webDir: 'dist'
+};
+
+export default config;
